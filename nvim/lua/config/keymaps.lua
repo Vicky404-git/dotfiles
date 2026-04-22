4921b1eaf6760db69f9ceb0599406bec1c7bee8e
vicky-404 <dhruwv499@gmail.com>
2026-04-23 02:19:23 +0530

    feat(config): add neovim config for explorer and terminal

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- explorer
+vim.keymap.set("n", "<leader>e", function()
+  require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+end, { desc = "Explorer" })
+-- terminal
+vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<CR>", { desc = "Terminal" })
